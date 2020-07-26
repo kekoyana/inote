c84595802cd264de6506ac58962e57a3a616eeff
kekoyana <kekoyana0@gmail.com>
2020-07-26 11:48:27 +0900

    fix note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,5 +2,6 @@
 
 class Note < ApplicationRecord
   belongs_to :tag
+  validates :tag_id, presence: true
   validates :body, presence: true, length: { maximum: 10_000 }
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,5 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:note, tag: tag, body: body).valid? }
+
+  context '正常入力のとき' do
+    let(:tag) { create(:tag) }
+    let(:body) { 'せいじょう' }
+
+    it { is_expected.to be_truthy }
+  end
+
+  context 'tagが空' do
+    let(:tag) { nil }
+    let(:body) { 'せいじょう' }
+
+    it { is_expected.to be_falsey }
+  end
+
+  context 'bodyが空' do
+    let(:tag) { create(:tag) }
+    let(:body) { nil }
+
+    it { is_expected.to be_falsey }
+  end
 end
